e198375b8657dbf09f9d4ba4b8e1330f667ee4c9
jen holiday <jenholiday@jens-MacBook-Pro.local>
2020-05-13 20:51:06 +0800

    adding starting project

---
+++ script.js
@@ -0,0 +1,31 @@
+function sayHello(){
+	console.log ("hello"); 
+}
+
+sayHello();
+
+
+var bye = function(){
+	console.log("Bye");
+}
+
+bye();
+
+function multiply(a,b){
+	// if (a>10 || b>10){
+	// 	return "that is too hard";
+	// } else {
+	// 	return a*b;
+	// }
+	return a*b;
+}
+
+multiply(12,3);
+
+
+function divide(c,d){
+	return c/d;
+}
+
+divide();
+
